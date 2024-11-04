1d42bb3bb24e0fb16ebe1b127ec0bf4bc2b7446d
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2024-11-04 16:01:44 -0300

    actualizo enviroment

--- src/components/configLocalHost.js
+++ src/components/configLocalHost.js
@@ -1,18 +1,13 @@
 const getBackendUrl = () => {
-  // Obtenemos el hostname actual
   const hostname = window.location.hostname;
-
-  // Verificamos si el usuario está accediendo desde localhost o una IP local (127.0.0.1)
   const isLocalhost = hostname === 'localhost' || hostname === '127.0.0.1';
 
-  // Determinamos la URL del backend según si es un entorno local o externo
   const BACKEND_URL = isLocalhost
-    ? 'http://localhost:5000'  // URL para el backend en desarrollo (servidor Node.js)
-    : process.env.BACKEND_URL_PROD;  // URL para producción (ngrok o servidor remoto)
+    ? 'http://localhost:5000'
+    : process.env.REACT_APP_BACKEND_URL_PROD;
 
   console.log("🚀 ~ BACKEND_URL:", BACKEND_URL);
 
-  // Retornamos la URL del backend
   return BACKEND_URL;
 };
 
